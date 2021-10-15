591e31c7770c16750447fc8a32adfd41beec9e72
Imtiaz <imtiazmiah@gmail.com>
2021-10-15 17:26:36 -0400

    Attempt at stacked bar, error on overwriting

--- src/Components/PokeGraphs.js
+++ src/Components/PokeGraphs.js
@@ -1,23 +1,43 @@
 import React from 'react'
 //import { HorizontalBar  } from 'react-chartjs-2'
-import {Bar, HorizontalBar} from 'react-chartjs-2';
+import { Bar, HorizontalBar } from 'react-chartjs-2';
 
 const PokeGraphs = (props) => {
-  
-  const {hp, attack, defense, specialAtt, specialDef, speed} = props.pokemon;
-  console.log('This is from pokegraphs: '+ attack)
+
+  const { hp, attack, defense, specialAtt, specialDef, speed } = props.pokemon;
+  console.log('This is from pokegraphs: ' + attack)
   return (
     <div>
       <Bar
-      type={'horizontalBar'}
+        type={'horizontalBar'}
         data={{
           labels: ['HP', 'Attack', "Defense", "Special Attack", "Special Defense", "Speed"],
           datasets: [{
-            data: [hp, attack, defense, specialAtt, specialDef, speed]
-          }]
+            data: [hp, attack, defense, specialAtt, specialDef, speed],
+            backgroundColor: "rgba(63,103,126,1)"
+          },
+          {
+            data: [21, 21, 21, 21, 21, 21],
+            backgroundColor: "rgba(163,103,126,1)"
+          },
+          {
+            data: [31, 31, 31, 31, 31, 31],
+            backgroundColor: "rgba(63,203,226,1)"
+          },
+          ]
         }
         }
-        options={{indexAxis: 'y'}}
+        // options={{
+        //   indexAxis: 'y',
+        //   scales: {
+        //     xAxes: [{
+        //       stacked: true
+        //     }],
+        //     yAxes: [{
+        //       stacked: true
+        //     }]
+        //   }
+        // }}
       />
     </div>
   )
